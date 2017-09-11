3bb8105ea257f5f77552c027da29b9968f8aaca3
Monika Urban <monika.urban1990@gmail.com>
2017-09-11 23:46:58 +0200

    in progres

--- szubienica.js
+++ szubienica.js
@@ -14,11 +14,46 @@ window.onload = start;
 
 var litery = new Array(35);
 
+litery[0]="A";
+litery[1]="Ą";
+litery[2]="B";
+litery[3]="C";
+litery[4]="Ć";
+litery[5]="D";
+litery[6]="E";
+litery[7]="Ę";
+litery[8]="F";
+litery[9]="G";
+litery[10]="H";
+litery[11]="I";
+litery[12]="J";
+litery[13]="K";
+litery[14]="L";
+litery[15]="Ł";
+litery[16]="M";
+litery[17]="N";
+litery[18]="Ń";
+litery[19]="O";
+litery[20]="Ó";
+litery[21]="P";
+litery[22]="Q";
+litery[23]="R";
+litery[24]="S";
+litery[25]="Ś";
+litery[26]="T";
+litery[27]="U";
+litery[28]="V";
+litery[29]="W";
+litery[30]="X";
+litery[31]="Y";
+litery[32]="Z";
+litery[33]="Ź";
+litery[34]="Ż";
 
 function start(){
 	var tresc_diva = "";
 	for (i=0; i<=34; i++) {
-		tresc_diva=tresc_diva+'<div class="litera">a</div>';
+		tresc_diva=tresc_diva+'<div class="litera">'+litery[i]+'</div>';
 		if ((i+1)%7==0) tresc_diva=tresc_diva+'<div style="clear:both;"</div>'
 	}
 		document.getElementById("alphabet").innerHTML = tresc_diva;
